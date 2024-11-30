dcee352d94c5dc5de8b42886a70177da8d8ab00e
Raul Espinoza M <raul.espmol@gmail.com>
2024-11-30 20:39:14 -0300

    crear Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import {Footer, Hero, Highlights, Offers, Products, Reviews, Services, Subscription} from './sections'
+import { Nav } from './components'
 
 const App = () => {
   return (
     <main className='relative'>
-      Nav
+      <Nav />
       <section className='xl:padding-1 wide:padding-r padding-b'>
         <Hero />
       </section>
