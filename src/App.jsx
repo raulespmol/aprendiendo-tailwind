e23a55f887ff239e50ec704853e1592b591bd795
Raul Espinoza M <raul.espmol@gmail.com>
2024-11-30 19:51:14 -0300

    crear componentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,35 @@
-export default function App() {
+import {Footer, Hero, Highlights, Offers, Products, Reviews, Services, Subscription} from './sections'
+
+const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <main className='relative'>
+      Nav
+      <section className='xl:padding-1 wide:padding-r padding-b'>
+        <Hero />
+      </section>
+      <section className='padding'>
+        <Products />
+      </section>
+      <section className='padding'>
+        <Highlights />
+      </section>
+      <section className='padding'>
+        <Services />
+      </section>
+      <section className='padding'>
+        <Offers />
+      </section>
+      <section className='padding bg-pale-blue'>
+        <Reviews />
+      </section>
+      <section className='padding-x sm:py-32 py-16 w-full'>
+        <Subscription />
+      </section>
+      <section className='bg-black padding-x padding-t pb-8'>
+        <Footer />
+      </section>
+    </main>
   )
 }
+
+export default App
---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Footer from './Footer'
+import Hero from './Hero'
+import Highlights from './Highlights'
+import Offers from './Offers'
+import Products from './Products'
+import Reviews from './Reviews'
+import Services from './Services'
+import Subscription from './Subscription'
+
+export {
+  Footer,
+  Hero,
+  Highlights,
+  Offers,
+  Products,
+  Reviews,
+  Services,
+  Subscription
+}
