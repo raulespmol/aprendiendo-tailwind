b47e08e25ddc6bc08594277fc62cc9db1828b994
Raul Espinoza M <raul.espmol@gmail.com>
2024-12-05 16:26:23 -0300

    [update] button component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,11 +1,11 @@
 const Button = ({label, icon}) => {
   return (
     <button className="flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none bg-coral-red rounded-full text-white">
-      <img 
+      {label}
+      {icon && <img 
         src={icon}
         className="mr-2 rounded-full w-5"  
-      /> 
-      {label}
+      />}
     </button>
   )
 }
