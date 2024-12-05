298c0b486a841a9300558a80649acef6f6faebdb
Raul Espinoza M <raul.espmol@gmail.com>
2024-12-05 17:40:13 -0300

    [create] reviews section

--- src/sections/Reviews.jsx
+++ src/sections/Reviews.jsx
@@ -1,6 +1,26 @@
+import { ReviewCard } from "../components"
+import { reviews } from "../constants"
+
 const Reviews = () => {
   return (
-    <div>Reviews</div>
+    <section className="max-container">
+      <h3 className="font-palanquin text-center text-4xl font-bold"> 
+        Qué opinan nuestros
+        <span className="text-coral-red "> Clientes </span>
+      </h3>
+      <p className="info-text m-auto mt-4 max-w-lg text-center">
+        Revisa historias genuinas de nuestros clientes satisfechos acerca de sus experiencias con nosotros.
+      </p>
+      <div className="mt-24 flex flex-1 justify-evenly items-center max-lg:flex-col gap-14">
+        {reviews.map(review => (
+          <ReviewCard 
+            key={review.customerName}
+            {...review}
+          />
+        ))}
+      </div>
+
+    </section>
   )
 }
 
