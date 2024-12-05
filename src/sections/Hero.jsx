7315ac02a0d85756727e3e78705e34e3c51b956f
Raul Espinoza M <raul.espmol@gmail.com>
2024-12-05 17:58:37 -0300

    [fix] hero section
    corregir key

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -43,8 +43,8 @@ const Hero = () => {
         />
 
         <div className="flex sm:gap-6 gap-4 absolute -bottom-[5%] sm:left-[10%] max-sm:px-6">
-          {shoes.map(shoe => (
-            <div key={shoe}>
+          {shoes.map((shoe, index) => (
+            <div key={index}>
               <ShoeCard 
                 imgURL={shoe}
                 changeBigShoeImage={(shoe) => setBigShoeImage(shoe)}
