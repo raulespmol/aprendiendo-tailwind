86a6418eb6a37c5723e805cf8b62506ff4c6df73
Raul Espinoza M <raul.espmol@gmail.com>
2024-12-11 21:38:48 -0300

    [fix] subscription title

--- src/sections/Subscription.jsx
+++ src/sections/Subscription.jsx
@@ -3,7 +3,7 @@ import { Button } from "../components"
 const Subscription = () => {
   return (
     <section className="max-container flex justify-between items-center max-lg:flex-col gap-10" id="contact-us">
-      <h3 className="text-4xl leading-[68px] lg:max-w-md font-palanquin font-bold sm:text-center">
+      <h3 className="text-4xl leading-[68px] lg:max-w-md font-palanquin font-bold max-sm:text-center">
         Suscríbete para recibir
         <span className="text-coral-red"> Ofertas </span>
         y Novedades
